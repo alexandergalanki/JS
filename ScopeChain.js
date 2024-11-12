7494730966732a57fb31ec529794b8f0eb22240f
Alexander Galanki <galankialex@gmail.com>
2024-11-12 21:49:49 +0530

    Add files via upload

---
+++ ScopeChain.js
@@ -0,0 +1,16 @@
+const myName="bobby";
+
+function first(){
+    const age=30;
+
+    if(age>=30){
+        const decade=3;
+        var millenial=true;
+    }
+    function second(){
+        const job="teacher";
+        console.log(`${myName} is a ${age}year's-old ${job},${millenial}`);
+    }
+    second();
+}
+first();
